8956d90971d057d4804e8fd38b752f73bf32b0f6
Kai Trepte <kai@kai.kaistein>
2020-01-24 12:20:04 -0500

    merged subgrid and window

--- run/run.sh
+++ run/run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Author:  K. Trepte
-# Date: 08.01.2020
+# Date: 24.01.2020
 porE_dir="$(dirname "$(pwd)")"
 
 ## Execute porE 
@@ -9,13 +9,9 @@ porE_dir="$(dirname "$(pwd)")"
 #$porE_dir/src/porE
 
 ## Execute porE_subgrid
-## Functionalities: OSA, GPA with sub-grid division (much faster)
-#$porE_dir/src/porE_subgrid
-
-## Execute porE_window
 ## Functionalities: OSA, GPA with sub-grid division (much faster), pore window evaluation (needs an output_PSD file as input)
-## Usage: First, generate an output_PSD file using the get_PSD exectuable. Then, you can use this executable to evaluate the pore windows
-$porE_dir/src/porE_window
+## Usage for pore window: First, generate an output_PSD file using the get_PSD exectuable. Then, you can use this executable to evaluate the pore windows
+$porE_dir/src/porE_subgrid
 
 ## Execute get_PSD
 ## Functionalities: Calculate the PSD with any desired accuracy
