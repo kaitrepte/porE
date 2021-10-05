55111911b66f55f04318dca332525d9ccbd3d442
kaitrepte <ktrepte@stanford.edu>
2021-10-05 14:51:38 -0700

    KT: Added benchmark data

--- examples/structures/cif/porE8/porE8.py
+++ examples/structures/cif/porE8/porE8.py
@@ -1,13 +1,14 @@
 from pore import porosity as p
 from porE.hea.HEA import HEA
+from pore import psd 
 from porE.io.ase2pore import *
 import glob
 
 cifs = glob.glob('*.cif')
 cifs.sort()
 f_out = 'benchmark.out'
-with open(f_out,'w',buffering=10) as o:
-    o.write('file P(HEA) P(OSA) P(GPAvoid) P(GPAacc)\n')
+with open(f_out,'w',buffering=1) as o:
+    o.write('file P(HEA) P(OSA) P(GPAvoid) P(GPAacc) PSD\n')
     for cif in cifs:
         # Perform HEA
         r1 = HEA(cif)
@@ -16,10 +17,17 @@ with open(f_out,'w',buffering=10) as o:
         xyz = 'pypore.xyz'
         # Perform OSA
         r2 = p.osa(xyz)
+        # perform PSD
+        no_pores,tmp1,tmp2,tmp3,tmp4 = psd.get_psd(xyz,200,2000)
+        pores           = tmp1[0:no_pores]
+        distr           = tmp2[0:no_pores]
+        pore_pos_cart   = tmp3[0:no_pores]
+        pore_pos_frac   = tmp4[0:no_pores]
         # Perform GPA
         probe_R = 1.2
         grid_density = 5.0
+        r3 = p.gpa_gridpera(xyz,probe_R,grid_density)
         # collect results
         r = [r1[0], r2[0], r3[0], r3[1]]
-        s = ' '.join([str(ri) for ri in r])
-        o.write(s)
+        #s = ' '.join([str(ri) for ri in r]+'\n')
+        o.write('{:30.30s} {:15.3f} {:15.3f} {:15.3f} {:15.3f} {} {} {}\n'.format(cif,r[0],r[1],r[2],r[3],no_pores,pores,distr))
